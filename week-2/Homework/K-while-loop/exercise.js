0ed182c81d529de8e93cb5010a9d3a843267fa81
carlossanchezheil1984 <carlos.sanchez.heil@gmail.com>
2020-08-11 13:23:39 +0200

    week2 and 3

---
+++ week-2/Homework/K-while-loop/exercise.js
@@ -0,0 +1,15 @@
+/* Write a function that:
+
+- Takes one number n as a parameter
+- Adds all numbers from 0 to n. For example, if the input is 3, the output should be 0 + 1 + 2 + 3
+- You should use a while loop
+
+*/
+
+let n = 10;
+
+function sumTillNum(num){
+	//your code here
+}
+
+console.log("Sum from 0 to " + n + " is: " + sumTillNum(n));
---
+++ week-2/Homework/M-array-loop/exercise.js
@@ -0,0 +1,15 @@
+/*
+	Write a function that receives an array of string, and console.log all strings that start with letter 'T'
+	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+*/
+
+
+const daysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
