123e098273b34aba10589b193e0046ac33521e57
Zaquiel <infozaquiel@gmail.com>
2020-09-08 14:47:19 +0200

    todos los eejercicios

--- week-3/Homework/H-array-methods-2/exercise2.js
+++ week-3/Homework/H-array-methods-2/exercise2.js
@@ -7,8 +7,11 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
-
+function capitalise(str) {
+  var mayuscula = str.split("").slice(0, 1)[0].toUpperCase();
+  var minuscula = str.split("").slice(1, str.length).join("");
+ return mayuscula + minuscula
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
